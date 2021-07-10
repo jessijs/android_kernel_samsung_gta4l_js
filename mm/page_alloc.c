930cdcfc217619df1629905a504cc0617978a366
Sultan Alsawaf <sultan@kerneltoast.com>
2021-07-09 22:20:42 -0700

    mm: vmpressure: Fix rampant inaccuracies caused by stale data usage
    After a period of intense memory pressure is over, it's common for
    vmpressure to still have old reclaim efficiency data accumulated from
    this time. When memory pressure starts to rise again, this stale data
    will factor into vmpressure's calculations, and can cause vmpressure to
    report an erroneously high pressure. The reverse is possible, too:
    vmpressure may report pressures that are erroneously low due to stale
    data that's been stored.
    
    Furthermore, since kswapd can still be performing reclaim when there are
    no failed memory allocations stuck in the page allocator's slow path,
    vmpressure may still report pressures when there aren't any memory
    allocations to satisfy. This can cause last-resort memory reclaimers to
    kill processes to free memory when it's not needed.
    
    To fix the rampant stale data, keep track of when there are processes
    utilizing reclaim in the page allocator's slow path, and reset the
    accumulated data in vmpressure when a new period of elevated memory
    pressure begins. Extra measures are taken for the kswapd issue mentioned
    above by ignoring all reclaim efficiency data reported by kswapd when
    there aren't any failed memory allocations in the page allocator which
    utilize reclaim.
    
    Note that since sr_lock can now be used from IRQ context, IRQs must be
    disabled whenever sr_lock is used to prevent deadlocks.
    
    Change-Id: Iaabb68bcbc88786e7f3c96960d3e660a74b2400f
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4575,6 +4575,7 @@ __alloc_pages_slowpath(gfp_t gfp_mask, unsigned int order,
 	int retry_loop_count = 0;
 	unsigned long jiffies_s = jiffies;
         bool woke_kswapd = false;
+        bool used_vmpressure = false;
         u64 utime, stime_s, stime_e, stime_d;
 
 	task_cputime(current, &utime, &stime_s);
@@ -4618,6 +4619,8 @@ restart:
 			atomic_long_inc(&kswapd_waiters);
 			woke_kswapd = true;
 		}
+		if (!used_vmpressure)
+			used_vmpressure = vmpressure_inc_users(order);
 		wake_all_kswapds(order, gfp_mask, ac);
 	}
 
@@ -4721,6 +4724,8 @@ retry:
 		goto nopage;
 
 	/* Try direct reclaim and then allocating */
+	if (!used_vmpressure)
+		used_vmpressure = vmpressure_inc_users(order);
 	page = __alloc_pages_direct_reclaim(gfp_mask, order, alloc_flags, ac,
 							&did_some_progress);
 	pages_reclaimed += did_some_progress;
@@ -4870,6 +4875,8 @@ got_pg:
 
 	if (woke_kswapd)
 		atomic_long_dec(&kswapd_waiters);
+	if (used_vmpressure)
+		vmpressure_dec_users();
 	if (!page)
 		warn_alloc(gfp_mask, ac->nodemask,
 				"page allocation failure: order:%u", order);
