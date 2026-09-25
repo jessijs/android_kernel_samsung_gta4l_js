288565a04f3f24070dbdda94f082023183abbb1c
Jessi <victorjsnogueira23@gmail.com>
2026-09-25 15:47:49 -0300

    Update build script for GKI 16 spoofing

--- build.sh
+++ build.sh
@@ -44,14 +44,15 @@ fi
     # 1. Configura as opções do kernel
     make $MAKE_ARGS gta4l_defconfig
 
-    # --- FORÇANDO O SPOOF GKI 6.12.38 PARA ANDROID 15/16/17 ---
-    # Roda uma compilação inicial rápida só para gerar os cabeçalhos
-    make $MAKE_ARGS init/ -j$(nproc --all) 2>/dev/null || true
+    # --- ROTA 3: BUSCA E DESTRUIÇÃO (O SPOOF DEFINITIVO) ---
+    echo "Aplicando injeção agressiva no código fonte do kernel..."
     
-    # Injeta a versão GKI de Android 16 diretamente no arquivo temporário
-    echo "Injetando a versão GKI spoof 6.12.38 no UTS_RELEASE..."
-    mkdir -p out/include/generated
-    echo '#define UTS_RELEASE "6.12.38-🩸LK_LittleKernel+"' > out/include/generated/utsrelease.h
+    # Em vez de brigar com os arquivos temporários, nós reescrevemos o código em C 
+    # que manda a informação para o Android (o cérebro do uname), substituindo 
+    # a macro UTS_RELEASE pela nossa string fixa do GKI (6.12.38).
+    
+    sed -i 's/UTS_RELEASE/"6.12.38-🩸LK_LittleKernel+"/g' init/version.c
+    
     # --------------------------------------------------------
 
     # 2. Continua a compilação pesada
@@ -79,10 +80,10 @@ fi
 
     # --- NOVO: Personalizando o nome no AnyKernel ---
     echo "Personalizando o nome do Kernel..."
-    sed -i 's/kernel.string=.*/kernel.string=🩸 LK_LittleKernel (Permissive) for SM-T500 by jessijs/g' anykernel.sh
+    sed -i 's/kernel.string=.*/kernel.string=🩸 LK_LittleKernel (Android 16 GKI spoof) by jessijs/g' anykernel.sh
     # ------------------------------------------------
 
-    ZIP_FILENAME=LK_LittleKernel_Permissive_gta4l_$(date +'%Y%m%d_%H%M%S')_${GIT_COMMIT_ID}.zip
+    ZIP_FILENAME=LK_LittleKernel_GKI16_gta4l_$(date +'%Y%m%d_%H%M%S')_${GIT_COMMIT_ID}.zip
     zip -r9 $ZIP_FILENAME ./* -x .git .gitignore out/ ./*.zip
     mv $ZIP_FILENAME ../
     cd ..
